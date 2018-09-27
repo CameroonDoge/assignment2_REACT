5e45488bd0895a6981f6fac239fb949e76572756
Cameran Dodge <cameran.dodge@gmail.com>
2018-09-26 20:27:46 -0400

    Added the ability to add and remove entries!

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import ViewBuilding from './components/ViewBuilding';
 import BuildingList from './components/BuildingList';
 import Credit from './components/Credit';
 import Paper from '@material-ui/core/Paper';
-import Card from '@material-ui/core/Card'
+import Card from '@material-ui/core/Card';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContentText from '@material-ui/core/DialogContentText';
+import Divider from '@material-ui/core/Divider'
 import { Grid } from '../node_modules/@material-ui/core';
-
-// const listStyle = {
-//   //height: 100,
-//   //width: 100,
-//   margin: "1em",
-//   // textAlign: 'center',
-//   display: 'inline-block'
-// };
 
 const pageStyle = {
   margin: "0.5em",
@@ -26,19 +26,29 @@ const cardStyle = {
   padding: "1.2em"
 };
 
-// const gridStyle ={
-//   width: "100%"
-// }
-
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       filterText: '',
-      selectedBuilding: 0
+      selectedBuilding: 0,
+      open: false,
+      newEntry:{
+        id: null,
+        code: '',
+        name: '',
+        coordinates:{
+          latitude: null,
+          longitude: null
+        },
+        address: ''
+      }
     };
     // somehow this has become very important...
     this.filterUpdate = this.filterUpdate.bind(this);
+    this.handleOpen = this.handleOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.codeUpdate = this.codeUpdate.bind(this);
   }
 
   filterUpdate(value) {
@@ -50,6 +60,53 @@ class App extends React.Component {
     //Here you will need to update the selectedBuilding property of state to the id passed into this function
     this.setState({selectedBuilding: id});
   }
+
+  handleOpen = () => {
+    this.setState({open: true});
+  }
+
+  handleClose = value => {
+    // this.setState({newBuilding: value, open: false});
+    this.setState({id: this.props.data.length+1})
+    this.setState({open: false});
+    this.props.data.push(this.state.newEntry);
+  }
+
+  handleCancel = value => {
+    this.setState({open: false});
+  }
+
+  codeUpdate = function(entity){
+    //this.setState({code: entity.target.value})
+    //this.setState({newEntry:{code: entity.target.value}})
+    var updatedEntry = this.state.newEntry;
+    updatedEntry.code = entity.target.value;
+    this.setState({newEntry:updatedEntry})
+  }
+
+  nameUpdate = function(entity){
+    var updatedEntry = this.state.newEntry;
+    updatedEntry.name = entity.target.value;
+    this.setState({newEntry:updatedEntry})
+  }.bind(this);
+
+  addressUpdate = function(entity){
+    var updatedEntry = this.state.newEntry;
+    updatedEntry.address = entity.target.value;
+    this.setState({newEntry:updatedEntry})
+  }.bind(this);
+
+  latitudeUpdate = function(entity){
+    var updatedEntry = this.state.newEntry;
+    updatedEntry.coordinates.latitude = entity.target.value;
+    this.setState({newEntry:updatedEntry})
+  }.bind(this);
+
+  longitudeUpdate = function(entity){
+    var updatedEntry = this.state.newEntry;
+    updatedEntry.coordinates.longitude = entity.target.value;
+    this.setState({newEntry:updatedEntry})
+  }.bind(this);
 
   render() {
     
@@ -71,17 +128,80 @@ class App extends React.Component {
                       <b>Code Building</b>
                     </td>
                   </tr>
-                  {/* <Paper style={listStyle}> */}
                   <BuildingList
                     data={this.props.data}
                     filterText={this.state.filterText}
                     onChange={this.selectedUpdate.bind(this)}
                   />
-                  {/* </Paper> */}
                 </table>
               </div>
             </div>
             <div className="column2">
+            <Button variant="outlined" color="primary" onClick={this.handleOpen}>Add New Building</Button>
+            <Dialog open={this.state.open} aria-labelledby="form-dialog-title" onClose={this.handleClose}>
+            <DialogTitle id="form-dialog-title">Add New Building</DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  Enter the building code, name, address, and if possible, coordinates.
+                </DialogContentText>
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  label="Code"
+                  type="filled"
+                  fullWidth
+                  value={this.state.code}
+                  onChange={this.codeUpdate}
+                />
+                {/* <input type="text"
+                placeholder="Code"
+                value={this.state.code}
+                onChange={this.codeUpdate}/> */}
+                <TextField
+                  margin="dense"
+                  label="Name"
+                  type="filled"
+                  fullWidth
+                  value={this.state.name}
+                  onChange={this.nameUpdate}
+                />
+                <TextField
+                  margin="dense"
+                  label="Address"
+                  type="filled"
+                  fullWidth
+                  value={this.state.address}
+                  onChange={this.addressUpdate}
+                />
+                <DialogContentText>
+                  <Divider/>
+                  Optional:
+                  <Divider/>
+                </DialogContentText>
+                <TextField
+                  margin="dense"
+                  label="latitude"
+                  type="filled"
+                  fullWidth
+                  value={this.state.latitude}
+                  onChange={this.latitudeUpdate}
+                />
+                <TextField
+                  margin="dense"
+                  label="longitude"
+                  type="filled"
+                  fullWidth
+                  value={this.state.longitude}
+                  onChange={this.longitudeUpdate}
+                />
+                
+              </DialogContent>
+              <DialogActions>
+                <Button color="primary" onClick={this.handleCancel}>Cancel</Button>
+                <Button color="primary" onClick={this.handleClose}>Submit</Button>
+              </DialogActions>
+            </Dialog>
+
             <Card style={cardStyle}>
               <ViewBuilding
               data={this.props.data}
@@ -100,3 +220,79 @@ class App extends React.Component {
 }
 
 export default App;
+
+// class newBuildingDialog extends React.Component{
+//   handleClose = () => {
+//     this.props.onClose(this.props.selectedValue)
+//   };
+
+//   handleEnterValue = value => {
+//     this.props.onClose(value);
+//   };
+
+//   constructor(props){
+//     super(props);
+//     this.state={
+//       //currMax: this.props.data.size(),
+//       open: false,
+//       newBuilding:{
+//         id: null,
+//         code: "",
+//         name: "",
+//         coordinates:{
+//           latitude: null,
+//           longitude: null
+//         },
+//         address: ""
+//       }
+//     }
+//     this.codeUpdate = this.codeUpdate.bind(this);
+//   }
+
+//   codeUpdate = function(entity){
+//     this.setState({code: entity.state.value})
+//   };
+
+//   nameUpdate = function(entity){
+//     this.setState({name: entity.state.value})
+//   };
+
+//   addressUpdate = function(entity){
+//     this.setState({address: entity.state.value})
+//   };
+
+//   latitudeUpdate = function(entity){
+//     this.setState({latitude: entity.state.value})
+//   };
+
+//   longitudeUpdate = function(entity){
+//     this.setState({longitude: entity.state.value})
+//   };
+
+//   render(){
+//     const {classes, onClose, selectedValue} = this.props;
+
+//     return(
+//       <Dialog onClose={this.handleClose} aria-labeledby="simple-dialog-title">
+//         <DialogTitle id="simple-dialog-title">Add New Building</DialogTitle>
+//         <DialogContent>
+//           <DialogContentText>
+//             Enter the building code, name, address, and if possible, coordinates
+//             </DialogContentText>
+//             <TextField
+//               autoFocus
+//               margin="dense"
+//               // id={currMax + 1}
+//               label="Code"
+//               type="text"
+//               fullWidth
+//             />
+//             </DialogContent>
+//           <DialogActions>
+//             <Button color="primary" onClick={this.handleClose}>Cancel</Button>
+//             <Button color="primary" onClick={this.handleClose}>Submit</Button>
+//           </DialogActions>
+//       </Dialog>
+//     );
+//   }
+// }
--- src/components/BuildingList.js
+++ src/components/BuildingList.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider'
 
 class BuilingList extends React.Component {
 
 	constructor(props){
 		super(props)
 		this.selectUpdate = this.selectUpdate.bind(this);
+		this.deleteEntry = this.deleteEntry.bind(this);
 	}
 
 	selectUpdate(id){
 		this.props.onChange(id);
+	}
+
+	deleteEntry(index){
+		this.props.data.splice(index, 1);
+		this.forceUpdate();
 	}
 
 	render() {
@@ -19,11 +25,15 @@ class BuilingList extends React.Component {
 			return element.name.toLowerCase().search(this.props.filterText.toLowerCase()) !== -1;
 		  }.bind(this));
 
-		const buildingList = data.map(directory => {
+		const buildingList = data.map((directory, index) => {
 			return (
-				<tr onClick={() => {this.selectUpdate(directory.id)}}
+				<tr
 					key={directory.id}>
-					<Button variant="outlined" color="primary">{directory.code} - {directory.name}</Button>
+					<Divider/>
+					<Button variant="outlined" color="secondary" onClick={() =>{this.deleteEntry(index)}}>Delete</Button>
+					<Divider/>
+					<Button variant="outlined" color="primary" onClick={() => {this.selectUpdate(directory.id)}}>{directory.code} - {directory.name}</Button>
+					<Divider/>
 				</tr>
 			);
 		});
